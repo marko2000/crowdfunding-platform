b96b832ca5656cbe7f757e1a123bef8fa8db51bd
marko2000 <mmarkom2000@gmail.com>
2022-12-31 00:44:48 +0100

    Build Campaign Details page

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -9,7 +9,7 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
   const navigate = useNavigate()
 
   const handleNavigate = (campaign) => {
-    navigate(`/campaign/details/${campaign.title}`, { state: campaign })
+    navigate(`/campaign-details/${campaign.title}`, { state: campaign })
   }
 
   return (
